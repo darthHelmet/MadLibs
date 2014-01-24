3e06520bf556b567a3216bb45fbc7e4fd9851cbe
bmarons <gamer43@gmail.com>
2014-01-23 22:48:54 -0500

    first commit

---
+++ MadLibs.py
@@ -0,0 +1,20 @@
+def madlib():
+
+	print "Welcome to Madlibs"
+
+
+	NOUN_PLURAL_1 = raw_input("Enter a Plural Noun: ")
+	PLACE = raw_input("Enter a Place: ")
+	NOUN_1 = raw_input("Enter a Noun: ")
+	NOUN_PLURAL_2 = raw_input("Enter a Plural Noun: ")
+	NOUN_2 = raw_input("Enter a Noun: ")
+	ADJECTIVE_1 = raw_input("Enter an Adjective: ")
+	VERB_1 = raw_input("Enter a Verb: ")
+	NUMBER = raw_input("Enter a Number: ")
+	ADJECTIVE_2 = raw_input("Enter an Adjective: ")
+	BODY_PART = raw_input("Enter a Body Part: ")
+	VERB_2 = raw_input("Enter a Body Verb: ")
+	print "Two" , NOUN_PLURAL_1, "both alike in dignity, In fair " , PLACE, ", where we lay our scene,From ancient" , NOUN_1, "break to new mutiny, Where civil blood makes civil hands unclean.From forth the fatal loins of these two foesA pair of star-cross`d" , NOUN_PLURAL_2, "take their life; Whole misadventured piteous overthrowsDo with their " , NOUN_2, "bury their parents` strife. The fearful passage of their" ,ADJECTIVE_1, "love,And the continuance of their parents` rage, Which, but their children`s end, nought could" , VERB_1, ",Is now the" , NUMBER, "hours` traffic of our stage; The which if you with " , ADJECTIVE_2, "" , BODY_PART, "attend,What here shall" , VERB_2, ", our toil shall strive to mend."
+
+
+madlib() 
